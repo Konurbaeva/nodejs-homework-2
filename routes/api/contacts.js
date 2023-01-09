d1c0ecc425f0f11d5800bad6cb8bfe0b948aeeb0
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-09 16:55:44 +0100

    added  GET /api/contacts/:id endpoint

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,13 +4,36 @@ const contacts = require("../../models/contacts")
 
 const router = express.Router()
 
+// возвращает массив всех контактов в json-формате со статусом 200
+
 router.get('/', async (req, res, next) => {
   const result = await contacts.listContacts();
+  res.status(200)
   res.json(result)
 })
 
+// GET /api/contacts/:id
+// Не получает body
+// Получает параметр id
+// вызывает функцию getById для работы с json-файлом contacts.json
+// если такой id есть, возвращает объект контакта в json-формате со статусом 200
+// если такого id нет, возвращает json с ключом "message": "Not found" и статусом 404
+
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+ 
+  try {
+    const { contactId } = req.params;
+
+    const contact = await contacts.getContactById(contactId);
+  
+    if(contact === null){
+   res.status(404).json({message: "Not found"})
+    } else{
+      res.json(contact)
+    }
+  } catch(err){
+    res.status(500).json({message: "Server error"} )
+  }
 })
 
 router.post('/', async (req, res, next) => {
