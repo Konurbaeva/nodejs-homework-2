119a1ff9fb1d30422b4706a510e33def2ccb3440
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-09 16:59:48 +0100

    modified GET endpoint

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -12,24 +12,16 @@ router.get('/', async (req, res, next) => {
   res.json(result)
 })
 
-// GET /api/contacts/:id
-// Не получает body
-// Получает параметр id
-// вызывает функцию getById для работы с json-файлом contacts.json
-// если такой id есть, возвращает объект контакта в json-формате со статусом 200
-// если такого id нет, возвращает json с ключом "message": "Not found" и статусом 404
-
 router.get('/:contactId', async (req, res, next) => {
- 
   try {
     const { contactId } = req.params;
 
     const contact = await contacts.getContactById(contactId);
   
     if(contact === null){
-   res.status(404).json({message: "Not found"})
+    res.status(404).json({message: "Not found"})
     } else{
-      res.json(contact)
+     res.status(200).json(contact)
     }
   } catch(err){
     res.status(500).json({message: "Server error"} )
