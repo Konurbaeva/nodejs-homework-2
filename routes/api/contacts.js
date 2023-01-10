37acad481436356d1bfe26d1242360cd6089f4dc
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-10 20:11:42 +0100

    added RequestError helper

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const contacts = require("../../models/contacts")
-const nanoid = require("nanoid");
+// const nanoid = require("nanoid");
+
+const { RequestError } = require("../../helpers")
 
 const Joi = require('joi');
 
@@ -12,14 +14,31 @@ router.get('/', async (req, res, next) => {
   res.status(200).json(result) 
 })
 
+// router.get('/:contactId', async (req, res, next) => {
+//   try {
+//     const { contactId } = req.params;
+
+//     const contact = await contacts.getContactById(contactId);
+  
+//     if(contact === null){
+//     res.status(404).json({message: "Not found"})
+//     } else{
+//      res.status(200).json(contact)
+//     }
+//   } catch(err){
+//     res.status(500).json({message: "Server error"} )
+//   }
+// })
+
 router.get('/:contactId', async (req, res, next) => {
   try {
     const { contactId } = req.params;
 
     const contact = await contacts.getContactById(contactId);
   
-    if(contact === null){
+    if(!contact){
     res.status(404).json({message: "Not found"})
+   //  throw RequestError(404, "Not found")
     } else{
      res.status(200).json(contact)
     }
@@ -34,21 +53,25 @@ router.get('/:contactId', async (req, res, next) => {
 // По результату работы функции возвращает объект с добавленным id {id, name, email, phone} и статусом 201
 
 router.post('/', async (req, res, next) => {
-  try {
-    const { name, email, phone } = req.body;
-    if (!name || !email || !phone) {
-      return res.status(400).json({ message: 'Missing required fields' });
-  } else {
+//   try {
+//     const { name, email, phone } = req.body;
+//     if (!name || !email || !phone) {
+//       return res.status(400).json({ message: 'Missing required fields' });
+//   } else {
   
-   const newContact = {  id: nanoid(), name, email, phone };
-   const newContactReqBody = await contacts.addContact(newContact)
+//    const newContact = {  id: nanoid(), name, email, phone };
+//    const newContactReqBody = await contacts.addContact(newContact)
 
-   //  const newContactReqBody = await contacts.addContact(req.body)
-   res.status(201).json(newContactReqBody)
-  }
-} catch(err) {
-    res.status(500).json({message: "Something went wrong"} )
-  }
+//    //  const newContactReqBody = await contacts.addContact(req.body)
+//    res.status(201).json(newContactReqBody)
+//   }
+// } catch(err) {
+//     res.status(500).json({message: "Something went wrong"} )
+//   }
+             
+const result = await contacts.addContact(req.body);
+
+res.status(201).json(result);
 })
 
 router.delete('/:contactId', async (req, res, next) => {
