c11ed66d1812bc44e382f0ebc4a9e945717d8d05
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-13 13:11:32 +0100

    removed unused comments

--- controllers/contacts/createContact.js
+++ controllers/contacts/createContact.js
@@ -9,14 +9,6 @@ const createContact = async (req, res) => {
 
   const result = await Contact.create(req.body)
   res.status(201).json(result)
-  
-  // const { error } = addSchema.validate(req.body);
-  // if(error){
-  //  throw RequestError(400, "Missing required field")
-  // } else {
-  //   const result = await contacts.addContact(req.body)
-  //   res.status(201).json(result)
-  // } 
 
 }
 
