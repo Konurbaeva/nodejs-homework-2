f4721ee82589489a07fb43e18dc29e79baf55270
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-13 13:18:17 +0100

    removed unused comments

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,8 +3,6 @@
 const Contact = require("../../models/contactsSchema")
 
 const getAllContacts = async (req, res) => {
-    // const result = await contacts.listContacts();
-    // res.status(200).json(result) 
     const result = await Contact.find()
 
     res.json(result)
