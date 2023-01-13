4c30450b2a465b7afb8d7bacb40cd969371d3395
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-13 13:40:21 +0100

    added required to contactSchema for validation

--- models/contactsSchema.js
+++ models/contactsSchema.js
@@ -1,11 +1,45 @@
 const { Schema, model } = require("mongoose")
 
+// const contactSchema = new Schema({
+//  name: String,
+//  email:String,
+//  phone: String,
+// })
+
 const contactSchema = new Schema({
- name: String,
- email:String,
- phone: String,
-})
+    name: {
+        type: String,
+        required: true
+      },
+    email: {
+        type: String,
+        required: true
+      },
+     phone: {
+        type: String,
+        required: true
+      }
+   })
+
 
 const Contact = model("contacts", contactSchema)
 
 module.exports = Contact;
+
+
+// {
+//     name: {
+//       type: String,
+//       required: [true, 'Set name for contact'],
+//     },
+//     email: {
+//       type: String,
+//     },
+//     phone: {
+//       type: String,
+//     },
+//     favorite: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   }
