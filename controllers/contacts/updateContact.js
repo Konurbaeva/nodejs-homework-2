de94548d555b6e51b6869a7fd1b4ccfe99a0cc5b
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-13 13:30:38 +0100

    replaced json by mongoose schema and model for updateContact

--- controllers/contacts/updateContact.js
+++ controllers/contacts/updateContact.js
@@ -7,19 +7,8 @@ const { addSchema } = require("../../schemas/contactsSchema")
 
 const updateContact = async (req, res) => {
 
-  // const { error } = addSchema.validate(req.body);
-  
-  // if(error){
-  //   throw RequestError(400, "Missing required field")
-  // } 
-
-  // const { contactId } = req.params;
-
-  // const result = await contacts.updateById(contactId, req.body)
-
-  // if(!result) {
-  //  throw RequestError(404, "Not found")
-  // } 
+  const result = await Contact.updateOne(req.body)
+  res.json(result)
   }
 
   module.exports = updateContact;
