6892a5c8efb46696d08abcb676a124be1826ac4e
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-14 11:24:29 +0100

    added userSchema

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect(DB_HOST)
 .catch(error => console.log(error.message))
 
 
+const authRouter = require("./routes/api/auth")
 const contactsRouter = require('./routes/api/contacts')
 const { listContacts } = require('./models/contacts')
 
@@ -26,6 +27,8 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+
+app.use('/api/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
