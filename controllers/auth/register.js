957709a402c67352ec71704eff9e2f1ca6e407ec
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-14 12:43:43 +0100

    added register controller

---
+++ controllers/auth/register.js
@@ -0,0 +1,23 @@
+const { User } = require("../../models/user")
+
+const { RequestError } = require("../../helpers")
+
+const register = async(req, res) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({email})
+
+    if(user){
+        throw RequestError(409, "Email in use")
+    }
+
+    const newUser = await User.create(req.body)
+
+    res.status(201).json({
+        name: newUser.name,
+        email: newUser.email
+    })
+
+}
+
+module.exports = register
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,15 @@
 const express = require("express")
 
+const { ctrlWrapper } = require("../../helpers")
+const { schemas } = require("../../models/user")
+
+const ctrl = require("../../controllers/auth")
+
 const router = express.Router()
+
+
+// router.post("/users/signup")
+// TODO implement validateBody
+router.post("/users/signup", validateBody(schemas.registerSchema), ctrlWrapper(ctrl.register))
 
 module.exports = router
