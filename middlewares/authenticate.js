3581a5eff10e46794154bd5f5b84a3af73cce124
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-15 13:47:43 +0100

    added jwt token and authenticate function

---
+++ middlewares/authenticate.js
@@ -0,0 +1,30 @@
+
+const jwt = require("jsonwebtoken")
+const { RequestError } = require("../helpers")
+
+const { User } = require("../models/user")
+
+const { SECRET_KEY } = process.env
+
+const authenticate = async(req, res, next) => {
+    const { authorization } = req.headers
+    const [bearer, token] = authorization.split(" ")
+
+    if(bearer!== "Bearer"){
+       next( RequestError(401))
+    }
+    try{
+        const { id } = jwt.verify(token, SECRET_KEY)
+        const user = await User.findById(id)
+
+        if(!user){
+            next(RequestError(401))
+        }
+
+    } catch(error) {
+      next(RequestError(401))
+    }
+
+}
+
+module.exports = authenticate;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,16 +3,16 @@ const express = require("express")
 const { ctrlWrapper } = require("../../helpers")
 const { schemas } = require("../../models/user")
 
-const { validateBody } = require("../../middlewares")
+const { validateBody, authenticate } = require("../../middlewares")
 
 const ctrl = require("../../controllers/auth")
 
 const router = express.Router()
 
-router.post("/users/signup", validateBody(schemas.registerSchema), ctrlWrapper(ctrl.register))
+router.post("/users/signup", authenticate, validateBody(schemas.registerSchema), ctrlWrapper(ctrl.register))
 
 
-router.post("/users/login", validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login))
+router.post("/users/login", authenticate, validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login))
 
 
 module.exports = router
