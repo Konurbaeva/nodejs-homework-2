0bc7479d619357881f5ece7f0079cab1bd65a16b
madina.konurbaeva <konurbaevamadina@gmail.com>
2023-01-17 19:33:29 +0100

    wip

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,7 @@ const express = require("express")
 const { ctrlWrapper } = require("../../helpers")
 const { schemas } = require("../../models/user")
 
-const { validateBody, authenticate } = require("../../middlewares")
+const { validateBody, authenticate, upload } = require("../../middlewares")
 
 const ctrl = require("../../controllers/auth")
 
@@ -16,5 +16,15 @@ router.post("/users/login", validateBody(schemas.loginSchema), ctrlWrapper(ctrl.
 router.get("/users/current", authenticate, ctrlWrapper(ctrl.getCurrent))
 
 router.post("/users/logout", authenticate, ctrlWrapper(ctrl.logout))
+
+// router.patch("/avatars", authenticate, upload.single("avatars"), ctrlWrapper(ctrl.updateAvatar))
+
+router.patch(
+   // "/users/avatars",
+    "/avatars",
+    authenticate,
+    upload.single("avatars"),
+    ctrlWrapper(ctrl.updateAvatar)
+  );
 
 module.exports = router
